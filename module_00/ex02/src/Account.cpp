2fc28724756680692cd2478a62289a19e0f4c308
marymota <dg.marciamota@gmail.com>
2022-06-19 19:31:58 +0100

    Finished and commented

--- module_00/ex02/src/Account.cpp
+++ module_00/ex02/src/Account.cpp
@@ -12,56 +12,82 @@ int Account::_totalNbWithdrawals = 0;
 // Constructor without parameter
 Account::Account(void)
 {
+	_displayTimestamp();
 	_accountIndex = _nbAccounts++;		// Account index is equal to total number of accounts incremented by 1
 	_amount = 0;						// The account is created with any deposit
 	_nbDeposits = 0;					// Number of deposits made
 	_nbWithdrawals = 0;					// Number of withrawals done
-
-	_displayTimestamp();
 }
 
 // Constructor with parameter
 Account::Account(int initial_deposit)
 {
 	_displayTimestamp();
-	std::cout << "index:" << _nbAccounts << ";" << "amount:" << _amount << ";created" << std::endl;
+	std::cout << "index:" << _nbAccounts << ";" << "amount:" << initial_deposit << ";created" << std::endl;
 
 	_accountIndex = _nbAccounts++;		// Account index is equal to total number of accounts incremented by 1
-	_amount = initial_deposit;			// The account is created with a initial deposit
+	_amount += initial_deposit;			// The account is created with a initial deposit
 	_nbDeposits = 0;					// Number of deposits made
 	_nbWithdrawals = 0;					// Number of withrawals done
-	_totalAmount += _amount;
+	_totalAmount += _amount;			
 }
 
-// Destructor
-Account::~Account(void)					// destructor
+//	Destructor
+Account::~Account(void)					// closes accounts
 {
-	;
-}
-
-void	Account::displayAccountsInfos()
-{
-	//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
 	_displayTimestamp();
-	std::cout << "accounts:" << _nbAccounts << ";" << "total:" << _totalAmount
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
+
+//	Prints Info of all accounts
+void	Account::displayAccountsInfos() 
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount
 	<< ";deposits:" << _totalNbDeposits << ";withrawals:" << _totalNbWithdrawals << std::endl;
 }
 
-void	Account::makeDeposit(int)
+//	Deposits money into an account incrementing the total amount and total number of deposits
+void	Account::makeDeposit(int deposit)
 {
 	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount
+	<< ";deposit:" << deposit << ";amount:" << _amount + deposit << ";nb_deposits:" << ++_nbDeposits << std::endl;
+	_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
 }
 
-bool	Account::makeWithdrawal(int)
+// Withdraws money from accounts decreasing the total amount and increasing the total number of withdrals 
+bool	Account::makeWithdrawal(int withdrawal)
 {
-	return (0);
+	_displayTimestamp();
+	if (withdrawal <= _amount)
+	{
+		std::cout << "index:" << _accountIndex << ";p_amount:" << _amount
+		<< ";withdrawal:" << withdrawal << ";amount:" << _amount - withdrawal 
+		<< ";nb_withdrawals:" << ++_nbWithdrawals << std::endl;
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+	}
+	else
+	{
+		std::cout << "index:" << _accountIndex << ";p_amount:" << _amount
+		<< ";withdrawal:refused" << std::endl;
+	}
+	return (withdrawal);
 }
 
+// Display info from an account
 void	Account::displayStatus() const
 {
 	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount
+	<< ";deposits:" << _nbDeposits << ";withrawals:" << _nbWithdrawals << std::endl;
 }
 
+// Display current time and date
 void	Account::_displayTimestamp( void )
 {
 	time_t now = time (0);
