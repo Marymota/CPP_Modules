02ba7c86df590090c9e55d9b8a1f204d3e790534
marymota <dg.marciamota@gmail.com>
2022-07-15 19:10:58 +0100

    module_01; ex04

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -1,64 +1,44 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
-std::string replace(std::string buff, std::string str, std::string new_str)
+int main (int argc, char *argv[]) 
 {
-	size_t start = 0;
-	size_t found = 0;
-	std::string newbuff;
-
-	while ((found = buff.find(str, start)) != std::string::npos)
+	// verify arguments
+	if (argc != 4) 
 	{
-		newbuff += buff.substr(start, found - start);
-		newbuff += new_str;
-		start = found + str.length();
-	}
-
-	return (newbuff);
-}
-
-int read_file(std::string filename, std::string str, std::string new_str)
-{
-	std::ifstream input;
-	std::ofstream output;
-	std::string buff;
-	size_t pos;
-	
-	input.open(filename.c_str(), std::ios::in);
-	output.open((filename + ".replace").c_str());
-	while (getline(input, buff))
-	{
-		pos = buff.find(str);
-
-		if (pos != std::string::npos)
-			std::cout << replace(buff, str, new_str) << std::endl;
-		else
-			std::cout << buff << std::endl;
-	}
-	input.close();
-	output.close();
-	return (0);
-}
-
-int main (int argc, char *argv[])
-{
-	if (argc != 4)
-	{
-		std::cerr << "Error\n Invalid number of arguments" << std::endl;
+		std::cerr << "Invalid number of arguments" << std::endl;
 		return (1);
 	}
-
+	// name files
 	std::string filename = argv[1];
-	std::string str = argv[2];
-	std::string new_str = argv[3];
-
-	if (str.empty() || new_str.empty())
-	{
-		std::cerr << "Error\n Invalid arguments" << std::endl;
+	filename += ".replace";
+	// open files
+	std::ifstream ifread(argv[1]);
+	std::ofstream ofwrite(filename.c_str());
+	if (!ifread.is_open() || !ofwrite.is_open()) {
+		std::cerr << "Invalid number of arguments" << std::endl;
 		return (1);
 	}
+	// read lines
+	std::string buff;
+	std::size_t pos;
+	std::string to_find = argv[1];
+	std::size_t len = to_find.length();
+	while (getline(ifread, buff)) 
+	{
+		// find string
+		int i = 0;
+		while ((pos = buff.find(argv[2], i)) != std::string::npos)
+		{
+			ofwrite << buff.substr(i, pos - i) << argv[3];
+			i = pos + len;
+		}
+		ofwrite << &buff[i] << std::endl;
+	}
 
-	read_file(filename, str, new_str);
-	return (0);
+	// close files
+	ifread.close();
+	ofwrite.close();
+
+	return 0;
 }
