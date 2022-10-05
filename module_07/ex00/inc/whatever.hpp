55ae0035c9edb39cd70be23335549bde3a4cd4e6
marymota <dg.marciamota@gmail.com>
2022-10-05 13:38:01 +0100

    module07 - ex00

---
+++ module_07/ex00/inc/whatever.hpp
@@ -0,0 +1,33 @@
+#ifndef __WHATEVER_HPP__
+#define __WHATEVER_HPP__
+
+#include <iostream>
+
+class Whatever {
+
+	public:
+		Whatever() {}
+		Whatever(Whatever& obj) { *this = obj; }
+		Whatever& operator=(Whatever& obj)  {*this = obj; return *this;}
+		~Whatever() {}
+};
+
+std::ostream& operator<<(std::ostream&, const Whatever&);
+
+// Does not return so it needs to pass arguments as reference 
+template <class T>
+void swap(T& x, T& y) 
+{
+	T t;
+	t = x;
+	x = y;
+	y = t;
+}
+
+template <class T>
+T min(T x, T y) { return (x<y?x:y); }
+
+template <class T>
+T max(T x, T y) { return (x>y?x:y); }
+
+#endif
